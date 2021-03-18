68eeba286379efd463d37b041b4e9ce4f209687d
Дмитрий Котелевец <dmkotelevets@Dmitrijs-MacBook-Pro.local>
2021-03-19 01:15:57 +0300

    TD-2: сборка работает, полученный файл dist/index.html открывается. Проблема с devServer, он пока не работает.

---
+++ src/components/App/App.tsx
@@ -0,0 +1,13 @@
+import React, { PureComponent } from "react";
+import './App.css';
+
+class App extends PureComponent {
+    public render() {
+        return (
+            <div>
+                new App
+            </div>
+        );
+    }
+}
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.tsx',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'app.js'
+    },
+    resolve: {
+        extensions: [".tsx", ".ts", ".js"]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: [
+                    {
+                        loader: 'ts-loader',
+                        options: {
+                            configFile: path.resolve(__dirname, 'tsconfig.json')
+                        }
+                    }
+                ],
+                exclude: /(node_modules)/
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.(png|jpg|svg|jpeg|gif)$/,
+                use: ['file-loader']
+            },
+            {
+                test: /\.html$/i,
+                loader: 'html-loader'
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html"
+        })
+    ],
+    devServer: {
+        contentBase: "./src",
+        historyApiFallback: true,
+        port: 8080
+    }
+}
