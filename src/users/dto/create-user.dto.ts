45b1a96f89733df33ac4c3ff9453086b57a579f9
Дмитрий Котелевец <dmkotelevets@gmail.com>
2021-05-13 10:48:19 +0300

    swagger, documentation for users

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,3 +1,6 @@
+import {ApiProperty} from "@nestjs/swagger";
+
 export class CreateUserDto {
+  @ApiProperty({example: 1, description: 'Уникальный id сервиса Yandex'})
   readonly ya_id: number;
 }
--- src/users/users.model.ts
+++ src/users/users.model.ts
@@ -1,4 +1,5 @@
 import {Column, DataType, Model, Table} from "sequelize-typescript";
+import {ApiProperty} from "@nestjs/swagger";
 
 interface UserCreationAttrs {
   ya_id: number;
@@ -6,9 +7,11 @@ interface UserCreationAttrs {
 
 @Table({tableName: 'users'})
 export class User extends Model<User, UserCreationAttrs> {
+  @ApiProperty({example: 1, description: 'Уникальный id сервиса TD'})
   @Column({type: DataType.INTEGER, unique: true, autoIncrement: true, primaryKey: true})
   id: number;
 
+  @ApiProperty({example: 1, description: 'Уникальный id сервиса Yandex'})
   @Column({type: DataType.INTEGER, unique: true, allowNull: false})
   ya_id: number;
 }
