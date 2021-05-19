970bb839750bec00e2436d78f11facf678eeb6fb
greyforce <greyforcedev@gmail.com>
2021-05-19 07:47:55 +0300

    TD-54 основные ручки для forums, themes, messages

--- backend/src/forum/forum.controller.ts
+++ backend/src/forum/forum.controller.ts
@@ -1,4 +1,88 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { CreateForumDto } from './dto/create-forum.dto';
+import { CreateMessageDto } from './dto/create-message.dto';
+import { CreateThemeDto } from './dto/create-theme.dto';
+import { UpdateMessageDto } from './dto/update-message.dto';
+import { UpdateThemeDto } from './dto/update-theme.dto';
+import { ForumService } from './forum.service';
 
 @Controller('forums')
-export class ForumController {}
+export class ForumController {
+  constructor(private forumService: ForumService) {}
+
+  /* THEME */
+
+  @Post('themes')
+  createTheme(@Body() themeDto: CreateThemeDto) {
+    return this.forumService.createTheme(themeDto)
+  }
+
+  @Put('themes/:id')
+  updateTheme(@Param('id') id: number, @Body() themeDto: UpdateThemeDto) {
+    return this.forumService.updateTheme(id, themeDto)
+  }
+
+  @Get('themes/:id')
+  getTheme(@Param('id') id: number) {
+    return this.forumService.getTheme(id);
+  }
+
+  @Get('themes')
+  getAllThemes() {
+    return this.forumService.getAllThemes()
+  }
+
+  @Delete('themes/:id')
+  deleteTheme(@Param('id') id: number) {
+    return this.forumService.deleteTheme(id);
+  }
+
+  /* MESSAGE */
+
+  @Post('messages')
+  createMessage(@Body() themeDto: CreateMessageDto) {
+    return this.forumService.createMessage(themeDto)
+  }
+
+  @Put('messages/:id')
+  updateMessage(@Param('id') id: number, @Body() themeDto: UpdateMessageDto) {
+    return this.forumService.updateMessage(id, themeDto)
+  }
+
+  @Get('messages/:id')
+  getMessage(@Param('id') id: number) {
+    return this.forumService.getMessage(id);
+  }
+
+  @Get('messages')
+  getAllMessages() {
+    return this.forumService.getAllMessages()
+  }
+
+  @Delete('messages/:id')
+  deleteMessage(@Param('id') id: number) {
+    return this.forumService.deleteMessage(id);
+  }
+
+  /* FORUM */
+
+  @Post()
+  createForum(@Body() forumDto: CreateForumDto) {
+    return this.forumService.createForum(forumDto)
+  }
+
+  @Get(':id')
+  getForum(@Param('id') id: number) {
+    return this.forumService.getForum(id);
+  }
+
+  @Get()
+  getAllForums() {
+    return this.forumService.getAllForums()
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: number) {
+    return this.forumService.deleteForum(id);
+  }
+}
--- backend/src/forum/forum.service.ts
+++ backend/src/forum/forum.service.ts
@@ -1,4 +1,83 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+import { CreateForumDto } from './dto/create-forum.dto';
+import { CreateMessageDto } from './dto/create-message.dto';
+import { CreateThemeDto } from './dto/create-theme.dto';
+import { UpdateMessageDto } from './dto/update-message.dto';
+import { UpdateThemeDto } from './dto/update-theme.dto';
+import { Forum } from './model/forum.model';
+import { Message } from './model/message.model';
+import { Theme } from './model/theme.model';
 
 @Injectable()
-export class ForumService {}
+export class ForumService {
+  constructor(
+    @InjectModel(Forum) private forumRepository: typeof Forum,
+    @InjectModel(Theme) private themeRepository: typeof Theme,
+    @InjectModel(Message) private messageRepository: typeof Message,
+  ) {}
+
+  async createForum(dto: CreateForumDto) {
+    const forum = await this.forumRepository.create(dto);
+    return forum;
+  }
+
+  async getForum(id: number) {
+    return this.forumRepository.findOne({ where: { id }, include: { all: true }});
+  }
+
+  async getAllForums() {
+    const forums = await this.forumRepository.findAll({ include: { all: true } });
+    return forums;
+  }
+
+  async deleteForum(id: number) {
+    return await this.forumRepository.destroy({ where: { id } })
+  }
+
+  async createTheme(dto: CreateThemeDto) {
+    const theme = await this.themeRepository.create(dto);
+    return theme;
+  }
+
+  async updateTheme(id: number, dto: UpdateThemeDto) {
+    await this.themeRepository.update(dto, { where: { id } });
+    return this.themeRepository.findOne({ where: { id } });
+  }
+
+  async getTheme(id: number) {
+    return this.themeRepository.findOne({ where: { id }, include: { all: true }});
+  }
+
+  async getAllThemes() {
+    const themes = await this.themeRepository.findAll({ include: { all: true } });
+    return themes;
+  }
+
+  async deleteTheme(id: number) {
+    return await this.forumRepository.destroy({ where: { id } })
+  }
+
+  async createMessage(dto: CreateMessageDto) {
+    const message = await this.messageRepository.create(dto);
+    return message;
+  }
+
+  async updateMessage(id: number, dto: UpdateMessageDto) {
+    await this.messageRepository.update(dto, { where: { id } });
+    return this.messageRepository.findOne({ where: { id } });
+  }
+
+  async getMessage(id: number) {
+    return this.messageRepository.findOne({ where: { id }, include: { all: true }});
+  }
+
+  async getAllMessages() {
+    const messages = await this.messageRepository.findAll({ include: { all: true } });
+    return messages;
+  }
+
+  async deleteMessage(id: number) {
+    return await this.forumRepository.destroy({ where: { id } })
+  }
+}
