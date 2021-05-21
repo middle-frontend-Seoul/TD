02a9e173ad85a54e2767538a1f35a80dfcaf0736
greyforce <greyforcedev@gmail.com>
2021-05-21 22:23:29 +0300

    TD-59 fix proxy forum url

--- src/constants/network.ts
+++ src/constants/network.ts
@@ -3,4 +3,5 @@ export const API_BASE_URL_SSR = 'https://ya-praktikum.tech/api/v2';
 export const IMAGE_SERVER_URL = 'https://ya-praktikum.tech';
 
 export const API_FORUM_BASE_URL = '/api-forum';
-export const API_FORUM_BASE_URL_SSR = 'http://localhost:7000/api-forum';
+export const API_FORUM_BASE_URL_SSR =
+  'https://seoul-test-td.herokuapp.com/api-forum';
